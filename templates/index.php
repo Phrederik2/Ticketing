abfd479b7f1fbc47c09f26871686846de5978486
Phrederik2 <phrederik2@gmail.com>
2022-05-26 15:44:43 +0200

    hjk

--- templates/index.php
+++ templates/index.php
@@ -4,7 +4,7 @@ style('ticketing', 'style');
 
 
 echo '<div id="app">';
-include_once ('templates/TicketingTemplate/BASE/before.php');
+include_once ('/var/www/html/nextcloud/apps/ticketing/templates/TicketingTemplate/index.php');
 
 
 //var_dump($_SERVER);
