9c576dc384f980d41bb97772646ae780c6790a46
Phrederik2 <phrederik2@gmail.com>
2022-05-26 16:22:52 +0200

    d6

--- templates/index.php
+++ templates/index.php
@@ -6,6 +6,19 @@ style('ticketing', 'style');
 echo '<div id="app">';
 include_once ('/var/www/html/nextcloud/apps/ticketing/templates/TicketingTemplate/index.php');
 
+echo 'Baselink:'.Tool::baselink().'<br>';
+
+echo '$_SERVER:<br>';
+foreach($_SERVER as $k=>$v){
+    echo '['.$k.']=>['.$v.']<br>';
+}
+
+echo '$__REQUEST:<br>';
+foreach($_REQUEST as $k=>$v){
+    echo '['.$k.']=>['.$v.']<br>';
+}
+
+
 
 //var_dump($_SERVER);
 $c = new customer_Frame();
