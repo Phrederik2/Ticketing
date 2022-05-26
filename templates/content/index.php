397c7112586289d6e462661ff8fd7265e409ea6c
Phrederik2 <phrederik2@gmail.com>
2022-05-26 15:17:21 +0200

    test template

--- templates/content/index.php
+++ templates/content/index.php
@@ -1,4 +1,1 @@
-<h1>Hello worldTOOOOOOOOOOOOOOOO</h1>
-<h1>Hello world 2</h1>
-<h1>Hello world 3</h1>
-<h1>new43</h1>
+test
--- templates/index.php
+++ templates/index.php
@@ -4,15 +4,6 @@ style('ticketing', 'style');
 ?>
 
 <div id="app">
-	<div id="app-navigation">
-		<?php print_unescaped($this->inc('navigation/index')); ?>
-		<?php print_unescaped($this->inc('settings/index')); ?>
-	</div>
-
-	<div id="app-content">
-		<div id="app-content-wrapper">
-			<?php print_unescaped($this->inc('content/index')); ?>
-		</div>
-	</div>
+	TEST Templates
 </div>
 
