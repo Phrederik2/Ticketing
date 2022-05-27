ea81e2f1d8829b09f5cdebd2a7ab43205d3e1e16
Phrederik2 <phrederik2@gmail.com>
2022-05-27 12:42:55 +0200

    j

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
 
 
-$('#F26b42c30c06ac6c80adea3219aaebd5a1136d8ea_Form_DataViewer2_Customer_26faea88eaa35bcb567e7a3f68388f87685b49ce_Option').val('EDIT_1');
+$('input[name="F26b42c30c06ac6c80adea3219aaebd5a1136d8ea_Form_DataViewer2_Customer_26faea88eaa35bcb567e7a3f68388f87685b49ce_Option"]').val('EDIT_1');
 //$('#F26b42c30c06ac6c80adea3219aaebd5a1136d8ea_Form_DataViewer2_Customer_26faea88eaa35bcb567e7a3f68388f87685b49ce').submit();
