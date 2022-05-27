acf3bf75f685a51d58e2f611279b261babd6c06a
Phrederik2 <phrederik2@gmail.com>
2022-05-27 14:12:13 +0200

    l

--- js/script.js
+++ js/script.js
@@ -3,8 +3,8 @@
 //$('input[name="F26b42c30c06ac6c80adea3219aaebd5a1136d8ea_Form_DataViewer2_Customer_26faea88eaa35bcb567e7a3f68388f87685b49ce_Option"]').val('EDIT_1');
 
 
-$list = $('[Form-Edit-Action]').on('click',function () {
-    alert($($this).atrt('Form-Edit-Action'));
+$('[Form-Edit-Action]').on('click',function () {
+    alert($($this).attr('Form-Edit-Action'));
     $(this).submit();
 });
 
