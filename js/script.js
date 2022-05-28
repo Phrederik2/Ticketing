a55e136c971be783bcb04a6718d15f1be5f01e50
Phrederik2 <phrederik2@gmail.com>
2022-05-28 10:28:21 +0200

    add track js

--- js/script.js
+++ js/script.js
@@ -2,18 +2,23 @@
 function addlistener(event){
     $('[Form-Action-'+event+']').on(event,function () {
         
-        if($(this).attr('LinkOption')!==undefined){
-            var nameO = $(this).attr('LinkOption');
-            $('[name="' + nameO + '"]').attr('value',$(this).attr('Action'));
-        }
-       
-        if($(this).attr('LinkForm')!==undefined){
-            var nameF = $(this).attr('LinkForm');
-            $('[name="' + nameF + '"]').submit();
-        }
+       launchEvent(this);
        
     });
 }
+
+function launchEvent(item){
+    if($(item).attr('LinkOption')!==undefined){
+        var nameO = $(item).attr('LinkOption');
+        $('[name="' + nameO + '"]').attr('value',$(item).attr('Action'));
+    }
+   
+    if($(item).attr('LinkForm')!==undefined){
+        var nameF = $(item).attr('LinkForm');
+        $('[name="' + nameF + '"]').submit();
+    }
+}
+
 addEventListener('click');
 addEventListener('change');
 
