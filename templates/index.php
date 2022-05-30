ad9a446b52e4d513cbf6f6dea598d5b41c25bafd
Phrederik2 <phrederik2@gmail.com>
2022-05-30 19:14:59 +0200

    test jquery

--- templates/index.php
+++ templates/index.php
@@ -1,6 +1,12 @@
 <?php
 script('ticketing', 'script');
+script('ticketing', 'assest2/ON-OFF-Toggle-Switches-Switcher/jquery.switcher');
+
+
+
+
 style('ticketing', 'style');
+style('ticketing', 'switcher');
 include_once ('/var/www/html/nextcloud/apps/ticketing/templates/TicketingTemplate/index.php');
 
 ?>
