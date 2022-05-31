cb998e755c0005dd5432afed95224d52add5d450
Phrederik2 <phrederik2@gmail.com>
2022-05-31 10:00:28 +0200

    add trumbowyg

--- templates/index.php
+++ templates/index.php
@@ -1,15 +1,26 @@
 <?php
 script('ticketing', 'script');
 script('ticketing', 'assets2/ON-OFF-Toggle-Switches-Switcher/js/jquery.switcher');
+
 //script('ticketing', 'assets2/bootstrap/js/bootstrap.min');
 //script('ticketing', 'assets2/bootstrap/js/bootstrap.min');
 
-
+script('ticketing', 'assets2/Trumbowyg/dist/trumbowyg.min');
+script('ticketing', 'assets2/Trumbowyg/dist/plugins/base64/trumbowyg.base64.min');
+script('ticketing', 'assets2/Trumbowyg/dist/plugins/colors/ui/trumbowyg.colors.min');
+script('ticketing', 'assets2/Trumbowyg/dist/plugins/fontfamily/trumbowyg.fontfamily.min');
+script('ticketing', 'assets2/Trumbowyg/dist/plugins/fontsize/trumbowyg.fontsize.min');
+script('ticketing', 'assets2/Trumbowyg/dist/plugins/history/trumbowyg.history.min');
+script('ticketing', 'assets2/Trumbowyg/dist/plugins/preformatted/trumbowyg.preformatted.min');
+script('ticketing', 'assets2/Trumbowyg/dist/plugins/resizimg/trumbowyg.resizimg.min');
+script('ticketing', 'assets2/Trumbowyg/dist/plugins/table/trumbowyg.table.min');
 
 
 style('ticketing', 'style');
 style('ticketing', 'switcher');
-style('ticketing', 'jqueryui');
+style('ticketing', 'trumbowyg.css');
+style('ticketing', 'trumbowyg.colors.css');
+
 include_once ('/var/www/html/nextcloud/apps/ticketing/templates/TicketingTemplate/index.php');
 
 ?>
