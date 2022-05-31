b4384cfbefa239cd7de57f749887ec7a2ee2ba6a
Phrederik2 <phrederik2@gmail.com>
2022-05-31 08:43:03 +0200

    k

--- templates/index.php
+++ templates/index.php
@@ -1,7 +1,8 @@
 <?php
 script('ticketing', 'script');
 script('ticketing', 'assets2/ON-OFF-Toggle-Switches-Switcher/js/jquery.switcher');
-script('ticketing', 'assets2/bootstrap/js/bootstrap.min');
+//script('ticketing', 'assets2/bootstrap/js/bootstrap.min');
+//script('ticketing', 'assets2/bootstrap/js/bootstrap.min');
 
 
 
